52c82776d01805ae57d718e11c6982585d57ecef
PawelCedzich <ced.paw.pc@gmail.com>
2023-04-09 17:34:20 +0200

    webapi need date fixes

--- webApi/cmd/webApi.go
+++ webApi/cmd/webApi.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"log"
+	"strconv"
+
 	"net/http"
 
 	"main.go/webApi/data"
@@ -13,39 +15,37 @@ type Page struct {
 	Body  data.BodyData
 }
 
-var templates = template.Must(template.ParseFiles("generator.html", "generate.html"))
+var templates = template.Must(template.ParseFiles("generate.html"))
 
 func loadPage(title string) (*Page, error) {
 	return &Page{Title: title}, nil
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	w.Header().Set("Content-Type", "text/html")
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-func generatorHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/generator/"):]
-	p, _ := loadPage(title)
-	renderTemplate(w, "generator", p)
-}
 
-func generate(w http.ResponseWriter, r *http.Request) {
+func generateHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/generate/"):]
 	p, _ := loadPage(title)
 
-	p.Body.Val1 = r.FormValue("val1")
-	p.Body.Val2 = r.FormValue("val2")
-	p.Body.Option = r.FormValue("option")
+	if r.Method == "POST" {
+		r.ParseForm()
+		p.Body.TableChoice = r.FormValue("options")
+		p.Body.AllTable, _ = strconv.ParseFloat(r.FormValue("allTable"), 64)
+		p.Body.SingleTable, _ = strconv.ParseFloat(r.FormValue("singleTable"), 64)
+	}
 
 	data.DbGenerate(p.Body)
 
-	renderTemplate(w, "generator", p)
+	renderTemplate(w, "generate", p)
 }
 
 func main() {
-	http.HandleFunc("/generator/", generatorHandler)
-	http.HandleFunc("/generate/", generate)
+	http.HandleFunc("/generate/", generateHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- webApi/data/data.go
+++ webApi/data/data.go
@@ -3,6 +3,11 @@ package data
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
 
 	_ "github.com/sijms/go-ora/v2"
 )
@@ -12,14 +17,9 @@ type Dane struct {
 }
 
 type BodyData struct {
-	Val1, Val2 string
-	Option     any
-}
-
-func newData(val1 string) Dane {
-	return Dane{
-		val1: val1,
-	}
+	AllTable    float64
+	SingleTable float64
+	TableChoice string
 }
 
 func DbGenerate(bodyData BodyData) {
@@ -38,10 +38,387 @@ func DbGenerate(bodyData BodyData) {
 		}
 	}()
 
-	fmt.Println(bodyData.Val1)
-	fmt.Println(bodyData.Val2)
-	fmt.Println(bodyData.Option)
-	//rows, err := db.Query("SELECT table_name, s101598 FROM user_tables ORDER BY owner, table_name")
-	//defer rows.Close()
+	if bodyData.AllTable != 0 {
+		//file create
+		hour, min, sec := time.Now().Clock()
+		fileName := fmt.Sprintf("inserted_AllTables_%d_%d_%d.txt", hour, min, sec)
+		f, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		warsztatID := CheckLastID("SELECT id_warsztatu FROM Warsztat WHERE id_warsztatu = (SELECT MAX(id_warsztatu) FROM Warsztat)", db)
+		autoZastepczeID := CheckLastID("SELECT id_auta_zastepczego FROM Warsztat WHERE id_auta_zastepczego = (SELECT MAX(id_auta_zastepczego) FROM auto_zastepcze)", db)
+		histAutaZastepczegoID := CheckLastID("SELECT ID_HISTORI_AUTA_ZASTEPCZEGO FROM Warsztat WHERE ID_HISTORI_AUTA_ZASTEPCZEGO = (SELECT MAX(ID_HISTORI_AUTA_ZASTEPCZEGO) FROM HISTORIA_AUTA_ZASTEPCZEGO)", db)
+		klientID := CheckLastID("SELECT ID_KLIENTA FROM Warsztat WHERE ID_KLIENTA = (SELECT MAX(ID_KLIENTA) FROM KLIENT)", db)
+		zamowieniaID := CheckLastID("SELECT ID_ZAMOWIENIA FROM Warsztat WHERE ID_ZAMOWIENIA = (SELECT MAX(ID_ZAMOWIENIA) FROM ZAMOWIENIA)", db)
+		magazyID := CheckLastID("SELECT ID_MAGAZYNU FROM Warsztat WHERE ID_MAGAZYNU = (SELECT MAX(ID_MAGAZYNU) FROM MAGAZYN)", db)
+		pracownikID := CheckLastID("SELECT ID_PRACOWNIKA FROM Warsztat WHERE ID_PRACOWNIKA = (SELECT MAX(ID_PRACOWNIKA) FROM PRACOWNIK_PIELEGNACJI)", db)
+		samochodKlientaID := CheckLastID("SELECT ID_SAMOCHODU_KLIENTA FROM Warsztat WHERE ID_SAMOCHODU_KLIENTA = (SELECT MAX(ID_SAMOCHODU_KLIENTA) FROM SAMOCHOD_KLIENTA)", db)
+		uslugaID := CheckLastID("SELECT ID_USLUGI FROM Warsztat WHERE ID_USLUGI = (SELECT MAX(ID_USLUGI) FROM USLUGI)", db)
+
+		for i := 1; i <= int(bodyData.AllTable); i++ {
+
+			idA := autoZastepczeID + i
+
+			data := time.Now().AddDate(0, 1, autoZastepczeID+idA)
+			var d string
+			if data.Month() < 10 && data.Day() < 10 {
+				d = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+			} else if data.Month() < 10 {
+				d = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+			} else if data.Day() < 10 {
+				d = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+			} else {
+
+				d = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+			}
+
+			queryStirng := fmt.Sprintf("INSERT INTO AUTO_ZASTEPCZE (ID_AUTA_ZASTEPCZEGO, NUMER_REJESTRACYJNY, TERMIN_PRZEGLADU, TERMIN_UBEZPIECZENIA) VALUES (%d, 'ok%d', '%s', '%s')", idA, idA, d, d)
+			_, err = db.Query(queryStirng)
+			if err != nil {
+				fmt.Printf("error in  1 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idH := histAutaZastepczegoID + i
+			var dH string
+			data = time.Now().AddDate(0, -2, histAutaZastepczegoID+idH)
+			if data.Month() < 10 && data.Day() < 10 {
+				dH = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+			} else if data.Month() < 10 {
+				dH = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+			} else if data.Day() < 10 {
+				dH = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+			} else {
+
+				dH = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+			}
+
+			queryStirng = fmt.Sprintf("INSERT INTO HISTORIA_AUTA_ZASTEPCZEGO(ID_HISTORI_AUTA_ZASTEPCZEGO, TERMIN_WYDANIA, TERMIN_ODEBRANIA, ZDARZENIE, AUTO_ZASTEPCZE_ID_AUTA_ZASTEPCZEGO) VALUES(%d, '%s', '%s', NULL , %d)", idH, dH, dH, idH)
+			_, err = db.Query(queryStirng)
+			if err != nil {
+				fmt.Printf("error in  2 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idK := klientID + i
+			numerLokalu := strconv.Itoa(rand.Intn(5))
+			if numerLokalu == "0" {
+				numerLokalu = "Null"
+			}
+			queryStirng = fmt.Sprintf("INSERT INTO KLIENT VALUES('%d', 'Imie_%d', 'NAzwisko_%d', 'Ulica_%d', %d, %s)", idK, idK, idK, idK, idK, numerLokalu)
+			_, err = db.Query(queryStirng)
+			if err != nil {
+				fmt.Printf("error in  3 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idW := warsztatID + i
+			queryStirng = fmt.Sprintf("INSERT INTO WARSZTAT VALUES (%d, 'miasto_%d', 'Ulica_%d', %d)", idW, idW, idW, idW)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 4 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idZ := zamowieniaID + i
+			czyZrealizowane := strconv.Itoa(rand.Intn(2))
+			if czyZrealizowane == "1" {
+				czyZrealizowane = "N"
+			} else {
+				czyZrealizowane = "T"
+			}
+			queryStirng = fmt.Sprintf("INSERT INTO ZAMOWIENIA VALUES(%d, '%s', '%s', %d)", idZ, czyZrealizowane, d, idZ)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 5 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idM := magazyID + i
+			var dM string
+			data = time.Now().AddDate(0, -2, magazyID+idM)
+			if data.Month() < 10 && data.Day() < 10 {
+				dM = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+			} else if data.Month() < 10 {
+				dM = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+			} else if data.Day() < 10 {
+				dM = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+			} else {
+
+				dM = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+			}
+			queryStirng = fmt.Sprintf("INSERT INTO MAGAZYN VALUES(%d, 'produkt_%d', 'dostawca_%d', %d, '%s', %d, %d)", idM, idM, idM, idM, dM, idM, idM)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 6 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idP := pracownikID + i
+			queryStirng = fmt.Sprintf("INSERT INTO PRACOWNIK_PIELEGNACJI VALUES (%d, '20310230%d', 'imie_%d', 'nazwisko_%d', '1%d', 'dzial_%d', %d)", idP, idP, idP, idP, idP, idP, idP)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 7 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			relID := CheckLastID("SELECT ID_KLIENTA FROM Warsztat WHERE ID_KLIENTA = (SELECT MAX(ID_KLIENTA) FROM KLIENT)", db)
+			idR := rand.Intn(relID)
+			queryStirng = fmt.Sprintf("INSERT INTO RELATION_17 VALUES (%d, %d)", idR, idR)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 8 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idSK := samochodKlientaID + i
+			queryStirng = fmt.Sprintf("INSERT INTO SAMOCHOD_KLIENTA VALUES(%d, 'DW03%d', 'model_%d', %d)", idSK, idSK, idSK, idSK)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 9 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+			idU := uslugaID + i
+			var dU string
+			data = time.Now().AddDate(0, 3, uslugaID+idU)
+			if data.Month() < 10 && data.Day() < 10 {
+				dU = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+			} else if data.Month() < 10 {
+				dU = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+			} else if data.Day() < 10 {
+				dU = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+			} else {
+
+				dU = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+			}
+			queryStirng = fmt.Sprintf("INSERT INTO USLUGA VALUES(%d, 'usluga_%d', '%s', %d, %d, %d)", idU, idU, dU, idU, idU, idU)
+			_, err = db.Exec(queryStirng)
+			if err != nil {
+				fmt.Printf("error in 10 qaury: %v", err)
+			}
+			f.WriteString(queryStirng + "\n")
+
+		}
+	}
+
+	if bodyData.SingleTable != 0 {
+
+		hour, min, sec := time.Now().Clock()
+		fileName := fmt.Sprintf("inserted_SingleTable_%d_%d_%d.txt", hour, min, sec)
+		f, err := os.Create(fileName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch bodyData.TableChoice {
+		case "AutoZas":
+			autoZastepczeID := CheckLastID("SELECT id_auta_zastepczego FROM Warsztat WHERE id_auta_zastepczego = (SELECT MAX(id_auta_zastepczego) FROM auto_zastepcze)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idA := autoZastepczeID + i
+
+				data := time.Now().AddDate(0, 1, autoZastepczeID+idA)
+				var d string
+				if data.Month() < 10 && data.Day() < 10 {
+					d = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+				} else if data.Month() < 10 {
+					d = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+				} else if data.Day() < 10 {
+					d = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+				} else {
+
+					d = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+				}
+
+				queryStirng := fmt.Sprintf("INSERT INTO AUTO_ZASTEPCZE (ID_AUTA_ZASTEPCZEGO, NUMER_REJESTRACYJNY, TERMIN_PRZEGLADU, TERMIN_UBEZPIECZENIA) VALUES (%d, 'ok%d', '%s', '%s')", idA, idA, d, d)
+				_, err = db.Query(queryStirng)
+				if err != nil {
+					fmt.Printf("error in  1 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "HisAutaZAs":
+			histAutaZastepczegoID := CheckLastID("SELECT ID_HISTORI_AUTA_ZASTEPCZEGO FROM Warsztat WHERE ID_HISTORI_AUTA_ZASTEPCZEGO = (SELECT MAX(ID_HISTORI_AUTA_ZASTEPCZEGO) FROM HISTORIA_AUTA_ZASTEPCZEGO)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+
+				idH := histAutaZastepczegoID + i
+				var dH string
+				data := time.Now().AddDate(0, -2, histAutaZastepczegoID+idH)
+				if data.Month() < 10 && data.Day() < 10 {
+					dH = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+				} else if data.Month() < 10 {
+					dH = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+				} else if data.Day() < 10 {
+					dH = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+				} else {
+
+					dH = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+				}
+
+				queryStirng := fmt.Sprintf("INSERT INTO HISTORIA_AUTA_ZASTEPCZEGO(ID_HISTORI_AUTA_ZASTEPCZEGO, TERMIN_WYDANIA, TERMIN_ODEBRANIA, ZDARZENIE, AUTO_ZASTEPCZE_ID_AUTA_ZASTEPCZEGO) VALUES(%d, '%s', '%s', NULL , %d)", idH, dH, dH, idH)
+				_, err = db.Query(queryStirng)
+				if err != nil {
+					fmt.Printf("error in  2 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "Klient":
+			klientID := CheckLastID("SELECT ID_KLIENTA FROM Warsztat WHERE ID_KLIENTA = (SELECT MAX(ID_KLIENTA) FROM KLIENT)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+
+				idK := klientID + i
+				numerLokalu := strconv.Itoa(rand.Intn(5))
+				if numerLokalu == "0" {
+					numerLokalu = "Null"
+				}
+				queryStirng := fmt.Sprintf("INSERT INTO KLIENT VALUES('%d', 'Imie_%d', 'NAzwisko_%d', 'Ulica_%d', %d, %s)", idK, idK, idK, idK, idK, numerLokalu)
+				_, err = db.Query(queryStirng)
+				if err != nil {
+					fmt.Printf("error in  3 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "Warsztat":
+			warsztatID := CheckLastID("SELECT id_warsztatu FROM Warsztat WHERE id_warsztatu = (SELECT MAX(id_warsztatu) FROM Warsztat)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idW := warsztatID + i
+				queryStirng := fmt.Sprintf("INSERT INTO WARSZTAT VALUES (%d, 'miasto_%d', 'Ulica_%d', %d)", idW, idW, idW, idW)
+				_, err = db.Exec(queryStirng)
+				if err != nil {
+					fmt.Printf("error in 4 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+
+			}
+		case "ZAmowienia":
+			zamowieniaID := CheckLastID("SELECT ID_ZAMOWIENIA FROM Warsztat WHERE ID_ZAMOWIENIA = (SELECT MAX(ID_ZAMOWIENIA) FROM ZAMOWIENIA)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idZ := zamowieniaID + i
+				czyZrealizowane := strconv.Itoa(rand.Intn(2))
+				if czyZrealizowane == "1" {
+					czyZrealizowane = "N"
+				} else {
+					czyZrealizowane = "T"
+				}
+				var dZ string
+				data := time.Now().AddDate(0, 3, zamowieniaID+idZ)
+				if data.Month() < 10 && data.Day() < 10 {
+					dZ = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+				} else if data.Month() < 10 {
+					dZ = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+				} else if data.Day() < 10 {
+					dZ = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+				} else {
+
+					dZ = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+				}
+				queryStirng := fmt.Sprintf("INSERT INTO ZAMOWIENIA VALUES(%d, '%s', '%s', %d)", idZ, czyZrealizowane, dZ, idZ)
+				_, err = db.Exec(queryStirng)
+				if err != nil {
+					fmt.Printf("error in 5 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "Magazyn":
+			magazyID := CheckLastID("SELECT ID_MAGAZYNU FROM Warsztat WHERE ID_MAGAZYNU = (SELECT MAX(ID_MAGAZYNU) FROM MAGAZYN)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idM := magazyID + i
+				var dM string
+				data := time.Now().AddDate(0, -2, magazyID+idM)
+				if data.Month() < 10 && data.Day() < 10 {
+					dM = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+				} else if data.Month() < 10 {
+					dM = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+				} else if data.Day() < 10 {
+					dM = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+				} else {
+
+					dM = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+				}
+				queryStirng := fmt.Sprintf("INSERT INTO MAGAZYN VALUES(%d, 'produkt_%d', 'dostawca_%d', %d, '%s', %d, %d)", idM, idM, idM, idM, dM, idM, idM)
+				_, err = db.Exec(queryStirng)
+				if err != nil {
+					fmt.Printf("error in 6 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "Pracownik":
+			pracownikID := CheckLastID("SELECT ID_PRACOWNIKA FROM Warsztat WHERE ID_PRACOWNIKA = (SELECT MAX(ID_PRACOWNIKA) FROM PRACOWNIK_PIELEGNACJI)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idP := pracownikID + i
+				queryStirng := fmt.Sprintf("INSERT INTO PRACOWNIK_PIELEGNACJI VALUES (%d, '20310230%d', 'imie_%d', 'nazwisko_%d', '1%d', 'dzial_%d', %d)", idP, idP, idP, idP, idP, idP, idP)
+				_, err = db.Exec(queryStirng)
+				if err != nil {
+					fmt.Printf("error in 7 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "SamochodKlienta":
+			samochodKlientaID := CheckLastID("SELECT ID_SAMOCHODU_KLIENTA FROM Warsztat WHERE ID_SAMOCHODU_KLIENTA = (SELECT MAX(ID_SAMOCHODU_KLIENTA) FROM SAMOCHOD_KLIENTA)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idSK := samochodKlientaID + i
+				queryStirng := fmt.Sprintf("INSERT INTO SAMOCHOD_KLIENTA VALUES(%d, 'DW03%d', 'model_%d', %d)", idSK, idSK, idSK, idSK)
+				_, err = db.Exec(queryStirng)
+				if err != nil {
+					fmt.Printf("error in 9 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+			}
+		case "USLUGA ":
+			uslugaID := CheckLastID("SELECT ID_USLUGI FROM Warsztat WHERE ID_USLUGI = (SELECT MAX(ID_USLUGI) FROM USLUGI)", db)
+
+			for i := 1; i < int(bodyData.SingleTable); i++ {
+				idU := uslugaID + i
+				var dU string
+				data := time.Now().AddDate(0, 3, uslugaID+idU)
+				if data.Month() < 10 && data.Day() < 10 {
+					dU = fmt.Sprintf("%d-0%d-0%d", data.Year(), data.Month(), data.Day())
+				} else if data.Month() < 10 {
+					dU = fmt.Sprintf("%d-0%d-%d", data.Year(), data.Month(), data.Day())
+				} else if data.Day() < 10 {
+					dU = fmt.Sprintf("%d-%d-0%d", data.Year(), data.Month(), data.Day())
+				} else {
+
+					dU = fmt.Sprintf("%d-%d-%d", data.Year(), data.Month(), data.Day())
+				}
+				queryStirng := fmt.Sprintf("INSERT INTO USLUGA VALUES(%d, 'usluga_%d', '%s', %d, %d, %d)", idU, idU, dU, idU, idU, idU)
+				_, err = db.Exec(queryStirng)
+				if err != nil {
+					fmt.Printf("error in 10 qaury: %v", err)
+				}
+				f.WriteString(queryStirng + "\n")
+
+			}
+		default:
+		}
+	}
+
+}
+
+func CheckLastID(query string, db *sql.DB) int {
+	lastID := 0
+	rows, err := db.Query("SELECT id_warsztatu FROM Warsztat WHERE id_warsztatu = (SELECT MAX(id_warsztatu) FROM Warsztat)")
+	defer rows.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for rows.Next() {
+		err := rows.Scan(&lastID)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return lastID
 }
